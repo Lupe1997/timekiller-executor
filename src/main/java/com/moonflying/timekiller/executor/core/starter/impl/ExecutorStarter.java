d21bf7f060c50b3282b74ca60bcc9a1bd72e2f7b
xunyi24 <liupf24@163.com>
2022-08-09 23:27:16 +0800

    feat: remove spring environment

--- src/main/java/com/moonflying/timekiller/executor/core/starter/impl/ExecutorStarter.java
+++ src/main/java/com/moonflying/timekiller/executor/core/starter/impl/ExecutorStarter.java
@@ -5,18 +5,10 @@ import com.moonflying.timekiller.executor.core.executor.AbstractScheduledTaskExe
 import com.moonflying.timekiller.executor.core.executor.impl.ScheduledTaskExecutor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.BeansException;
-import org.springframework.beans.factory.DisposableBean;
-import org.springframework.beans.factory.SmartInitializingSingleton;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.ApplicationContextAware;
-import org.springframework.core.MethodIntrospector;
-import org.springframework.core.annotation.AnnotatedElementUtils;
 import com.moonflying.timekiller.executor.annotation.ScheduledTask;
 import com.moonflying.timekiller.executor.core.starter.AbstractExecutorStarter;
 
 import java.lang.reflect.Method;
-import java.util.Map;
 import java.util.ServiceLoader;
 
 /**
@@ -42,40 +34,16 @@ public class ExecutorStarter extends AbstractExecutorStarter {
     }
 
     private void initJobHandlerMethodRepository() {
+        // get task classes by Java SPI
         ServiceLoader<ScheduledTaskClass> taskClasses = ServiceLoader.load(ScheduledTaskClass.class);
-
-
         // init job handler from method
-        String[] beanNames = applicationContext.getBeanNamesForType(Object.class, false, true);
-        for (String beanName : beanNames) {
-            Object bean = applicationContext.getBean(beanName);
-
-            Map<Method, ScheduledTask> annotatedMethods;
-            try {
-                annotatedMethods = MethodIntrospector.selectMethods(
-                        bean.getClass(),
-                        new MethodIntrospector.MetadataLookup<ScheduledTask>() {
-                            @Override
-                            public ScheduledTask inspect(Method method) {
-                                return AnnotatedElementUtils.findMergedAnnotation(method, ScheduledTask.class);
-                            }
-                        }
-                );
-            } catch (Throwable ex) {
-                logger.error("Occur an exception when resolve ScheduledTask annotatedMethods for bean[" + beanName + "].", ex);
-                throw ex;
-            }
-
-            if (annotatedMethods.isEmpty()) continue;
-
-            for (Map.Entry<Method, ScheduledTask> methodScheduledTaskEntry : annotatedMethods.entrySet()) {
-                Method executeMethod = methodScheduledTaskEntry.getKey();
-                if (executeMethod.getParameterCount() > 0)
-                    throw new RuntimeException("The method: " + executeMethod.getName() + " of scheduled task shouldn't have parameters");
-                ScheduledTask scheduledTask = methodScheduledTaskEntry.getValue();
-
-                // register
-                registerTaskHandler(scheduledTask, bean, executeMethod);
+        for (ScheduledTaskClass taskClass : taskClasses) {
+            Method[] methods = taskClass.getClass().getDeclaredMethods();
+            for (Method method : methods) {
+                ScheduledTask scheduledTask = method.getAnnotation(ScheduledTask.class);
+                if (scheduledTask != null) {
+                    registerTaskHandler(scheduledTask, taskClass, method);
+                }
             }
         }
     }
