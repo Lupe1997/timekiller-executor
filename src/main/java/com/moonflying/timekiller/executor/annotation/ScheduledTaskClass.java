9c5cde0e6f4ef720ed4bdb656a2533980713ab1a
Lupe97 <liupf24@163.com>
2022-08-11 17:26:32 +0800

    refactor: abandon taking Java Spi approach to get all tasks

--- src/main/java/com/moonflying/timekiller/executor/annotation/ScheduledTaskClass.java
+++
@@ -1,8 +0,0 @@
-package com.moonflying.timekiller.executor.annotation;
-
-/**
- * @Author ffei
- * @Date 2022/8/8 22:04
- */
-public interface ScheduledTaskClass {
-}
--- src/main/java/com/moonflying/timekiller/executor/core/starter/impl/SimpleExecutorStarter.java
+++ src/main/java/com/moonflying/timekiller/executor/core/starter/impl/SimpleExecutorStarter.java
@@ -1,30 +1,32 @@
 package com.moonflying.timekiller.executor.core.starter.impl;
 
-import com.moonflying.timekiller.executor.annotation.ScheduledTaskClass;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import com.moonflying.timekiller.executor.annotation.ScheduledTask;
 import com.moonflying.timekiller.executor.core.starter.AbstractExecutorStarter;
 
 import java.lang.reflect.Method;
-import java.util.ServiceLoader;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @Author ffei
  * @Date 2021/11/20 23:26
- * Get scheduled tasks by Java SPI
- * TODO 删除了Spring，需要自己实现初始化启动代码（删除了SmartInitializingSingleton->afterSingletonsInstantiated）
  * TODO 删除了 DisposableBean，需要自己实现对象销毁后调用stop方法
  */
-public class SPIExecutorStarter extends AbstractExecutorStarter {
-    private static final Logger logger = LoggerFactory.getLogger(SPIExecutorStarter.class);
+public class SimpleExecutorStarter extends AbstractExecutorStarter {
+    private static final Logger logger = LoggerFactory.getLogger(SimpleExecutorStarter.class);
+
+    private final List<Object> scheduledTasks = new ArrayList<>();
+
+    public void setScheduledTasks(List<Object> tasks) {
+        scheduledTasks.addAll(tasks);
+    }
 
     @Override
     public void start() {
-        // get task classes by Java SPI
-        ServiceLoader<ScheduledTaskClass> taskClasses = ServiceLoader.load(ScheduledTaskClass.class);
         // init job handler from method
-        for (ScheduledTaskClass taskClass : taskClasses) {
+        for (Object taskClass : scheduledTasks) {
             Method[] methods = taskClass.getClass().getDeclaredMethods();
             for (Method method : methods) {
                 ScheduledTask scheduledTask = method.getAnnotation(ScheduledTask.class);
